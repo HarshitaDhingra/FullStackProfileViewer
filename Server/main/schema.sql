1bb64db0985d14332d3d0c8553aa984995e62100
iqbal125 <iqbal125@yahoo.com>
2019-03-17 17:22:01 -0700

    Update schema.sql

--- Server/main/schema.sql
+++ Server/main/schema.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users (
   uid SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE,
@@ -8,14 +7,16 @@ CREATE TABLE users (
   last_login DATE
 );
 
-
 CREATE TABLE posts (
   pid SERIAL PRIMARY KEY,
   title VARCHAR(255),
   body VARCHAR,
+  search_vector TSVECTOR,
   user_id INT REFERENCES users(uid),
   author VARCHAR REFERENCES users(username),
-  date_created TIMESTAMP
+  date_created TIMESTAMP,
+  like_user_id INT[] DEFAULT ARRAY[]::INT[],
+  likes INT DEFAULT 0
 );
 
 CREATE TABLE comments (
@@ -24,7 +25,7 @@ CREATE TABLE comments (
   author VARCHAR REFERENCES users(username),
   user_id INT REFERENCES users(uid),
   post_id INT REFERENCES posts(pid),
-  date_created TIMESTAMP
+  date_created TIMESTAMP,
 );
 
 
